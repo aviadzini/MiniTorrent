120f691bd1de00f44c78b53ba9f89413db918d00
Aviad Zini <aviadzini@gmail.com>
2017-03-01 14:40:24 +0200

    Massive work on client-sever

---
+++ MiniTorrentClient/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiniTorrentClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Socket clientSocket;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                clientSocket.BeginConnect(new IPEndPoint(IPAddress.Loopback, 8005), new AsyncCallback(ConnectCallback), null);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBoxResult result = MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void loginB_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.Compare(usernameTB.Text, "") == 0 || string.Compare(passwordTB.Text, "") == 0)
+            {
+                usernameTB.Clear();
+                passwordTB.Clear();
+
+                MessageBoxResult result = MessageBox.Show("Cannot be empty", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            else
+            {
+                try
+                {
+                    byte[] buffer = Encoding.ASCII.GetBytes(usernameTB.Text);
+                    clientSocket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(SendCallback), null);
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBoxResult result = MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void SendCallback(IAsyncResult AR)
+        {
+            clientSocket.EndSend(AR);
+        }
+
+        private void ConnectCallback(IAsyncResult AR)
+        {
+            try
+            {
+                clientSocket.EndConnect(AR);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBoxResult result = MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ MiniTorrentServer/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniTorrentServer
+{
+    public partial class Form1 : Form
+    {
+        private Socket serverSocket;
+        private Socket clientSocket;
+
+        private byte[] buffer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            startServer();
+        }
+
+        private void startServer()
+        {
+            try
+            {
+                serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                serverSocket.Bind(new IPEndPoint(IPAddress.Any, 8005));
+                serverSocket.Listen(0);
+                serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult AR)
+        {
+            try
+            {
+                clientSocket = serverSocket.EndAccept(AR);
+                buffer = new byte[clientSocket.ReceiveBufferSize];
+                clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            try
+            {
+                int received = clientSocket.EndReceive(AR);
+                Array.Resize(ref buffer, received);
+                string text = Encoding.ASCII.GetString(buffer);
+                serverTB.Text += text + "\r\n";
+                Array.Resize(ref buffer, clientSocket.ReceiveBufferSize);
+                clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
